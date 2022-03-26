a0f339eb4b4173e76a8998f7b2d77184b5cf1435
jjubbu <hogugugut4102.off@gmail.com>
2022-03-26 23:14:54 +0900

    0326숙제

---
+++ 0326HW/1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+
+    char main[] = "int main(void)\n{\n   return 0;\n}";
+    printf("%s\n", main);
+
+}
---
+++ 0326HW/11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    double pie = 3.14159265;
+    printf("pi = %.2f\n", pie);
+    printf("pi = %.4f\n", pie);
+    printf("pi = %.6f\n", pie);
+    printf("pi = %.8f\n", pie);
+    printf("pi = %e\n", pie);
+    return 0;
+}
